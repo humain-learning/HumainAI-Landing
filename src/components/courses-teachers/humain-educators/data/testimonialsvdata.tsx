a9ffc62c9970553e9055dcd6db8f8539b1067dac
Raghav Kaul <raghav.kaul@humainlearning.ai>
2026-01-27 22:03:30 +0530

    update

--- src/components/courses-teachers/humain-educators/data/testimonialsvdata.tsx
+++ src/components/courses-teachers/humain-educators/data/testimonialsvdata.tsx
@@ -1,38 +1,27 @@
 export const testimonialVideos = [
     {
         id: 1,
-        url: "https://player.vimeo.com/video/1156799992",
-        title: "Module 1",
-        description: "The AI Revolution"
+        url: "https://player.vimeo.com/video/1158712718",
+        title: "Primary School",
     },
     {
         id: 2,
-        url: "https://player.vimeo.com/video/1156796118",
-        title: "Module 2",
-        description: "AI for Planning and Assessment"
+        url: "https://player.vimeo.com/video/1158712680",
+        title: "Sr. Secondary",
     },
     {
         id: 3,
-        url: "https://player.vimeo.com/video/1156796232",
-        title: "Module 3",
-        description: "AI for Teaching and Learning Materials"
+        url: "https://player.vimeo.com/video/1158712757",
+        title: "Senior",
     },
     {
         id: 4,
-        url: "https://player.vimeo.com/video/1156796176",
-        title: "Module 4",
-        description: "AI for Creativity"
+        url: "https://player.vimeo.com/video/1158712575",
+        title: "Junior",
     },
     {
         id: 5,
-        url: "https://player.vimeo.com/video/1156796033",
-        title: "Module 5",
-        description: "Responsible and Ethical AI Use"
+        url: "https://player.vimeo.com/video/1158712622",
+        title: "Pre Primary",
     },
-    {
-        id: 6,
-        url: "https://player.vimeo.com/video/1156796077",
-        title: "Module 6",
-        description: "AI forReflection and Feedback"
-    }
 ];
