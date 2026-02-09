f67a7143bb98087ad7493989f91c47d4ca2d9da2
Raghav Kaul <raghav.kaul@humainlearning.ai>
2026-02-09 11:06:06 +0530

    Update videotestimonials.tsx

---
+++ src/components/courses-teachers/humain-educators/data/videotestimonials.tsx
@@ -0,0 +1,18 @@
+export const videoTestimonials = [
+    {
+        id: 1,
+        url: "https://player.vimeo.com/video/1163141228",
+    },
+    {
+        id: 2,
+        url: "https://player.vimeo.com/video/1162440274",
+    },
+    {
+        id: 3,
+        url: "https://player.vimeo.com/video/1162440350",
+    },
+    {
+        id: 4,
+        url: "https://player.vimeo.com/video/1162440456",
+    },
+];
