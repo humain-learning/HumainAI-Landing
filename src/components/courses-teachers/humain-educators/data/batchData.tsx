d6ca73e29b35a8d3ae2161496d9d2d08f07d8649
Raghav Kaul <raghav.kaul@humainlearning.ai>
2026-03-17 11:58:52 +0530

    Update batchData.tsx

--- src/components/courses-teachers/humain-educators/data/batchData.tsx
+++ src/components/courses-teachers/humain-educators/data/batchData.tsx
@@ -1,54 +1,5 @@
 const Batches = [
     {
-        name: 'Early March Batch',
-        startDate: "7th March",
-        limitedSeats: true,
-        soldOut:false,
-        itinerary: [
-            {
-                date: '7th Mar',
-                day: 'Saturday',
-                timing: '5PM-7PM'
-            },
-            {
-                date: '8th Mar',
-                day: 'Sunday',
-                timing: '5PM-7PM'
-            },
-            {
-                date: '14th Mar',
-                day: 'Saturday',
-                timing: '5PM-7PM'
-            },
-            {
-                date: '15th Mar',
-                day: 'Sunday',
-                timing: '5PM-7PM'
-            },
-            {
-                date: '21st Mar',
-                day: 'Saturday',
-                timing: '5PM-7PM'
-            },
-            {
-                date: '22nd Mar',
-                day: 'Sunday',
-                timing: '5PM-7PM'
-            },
-            {
-                date: '23rd Mar',
-                day: 'Monday',
-                timing: '5PM - Doubt Clearing'
-            },
-            {
-                date: '27th Mar',
-                day: 'Friday',
-                timing: '5PM - Graduation'
-            }
-        ]
-        
-    },
-    {
         name: 'April Batch',
         startDate: "4th April",
         limitedSeats: false,
@@ -97,56 +48,6 @@ const Batches = [
         ]
         
     },
-    {
-        name: 'Late March Batch',
-        startDate: "21st March",
-        limitedSeats: false,
-        soldOut:true,
-        itinerary: [
-            {
-                date: '21st Mar',
-                day: 'Saturday',
-                timing: '5PM-7PM'
-            },
-            {
-                date: '22nd Mar',
-                day: 'Sunday',
-                timing: '5PM-7PM'
-            },
-            {
-                date: '28th Mar',
-                day: 'Saturday',
-                timing: '5PM-7PM'
-            },
-            {
-                date: '29th Mar',
-                day: 'Sunday',
-                timing: '5PM-7PM'
-            },
-            {
-                date: '4th Apr',
-                day: 'Saturday',
-                timing: '5PM-7PM'
-            },
-            {
-                date: '5th Apr',
-                day: 'Sunday',
-                timing: '5PM-7PM'
-            },
-            {
-                date: '6th Apr',
-                day: 'Monday',
-                timing: '5PM - Doubt Clearing'
-            },
-            {
-                date: '10th Apr',
-                day: 'Friday',
-                timing: '5PM - Graduation'
-            }
-        ]
-        
-    },
-
 ];
 
 if (Batches.length === 1) {
