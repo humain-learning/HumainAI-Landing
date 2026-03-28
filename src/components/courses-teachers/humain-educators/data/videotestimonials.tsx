5c45194055962ed33c5cdef008a5026529e7b6fe
Raghav Kaul <raghav.kaul@humainlearning.ai>
2026-03-28 10:39:28 +0530

    Update videotestimonials.tsx

--- src/components/courses-teachers/humain-educators/data/videotestimonials.tsx
+++ src/components/courses-teachers/humain-educators/data/videotestimonials.tsx
@@ -1,14 +1,42 @@
 export const videoTestimonials = [
-    {
-        id: 1,
-        url: "https://player.vimeo.com/video/1163216935",
-    },
-    {
-        id: 2,
-        url: "https://player.vimeo.com/video/1163216763",
-    },
-    {
-        id: 3,
-        url: "https://player.vimeo.com/video/1163216558",
-    },
+	{
+		id: 1,
+		url: "https://player.vimeo.com/video/1163216935",
+	},
+	{
+		id: 2,
+		url: "https://player.vimeo.com/video/1176081774",
+	},
+	{
+		id: 3,
+		url: "https://player.vimeo.com/video/1176081511",
+	},
+	{
+		id: 4,
+		url: "https://player.vimeo.com/video/1176080901",
+	},
+	{
+		id: 5,
+		url: "https://player.vimeo.com/video/1176080798",
+	},
+	{
+		id: 6,
+		url: "https://player.vimeo.com/video/1176080647",
+	},
+	{
+		id: 7,
+		url: "https://player.vimeo.com/video/1176080420",
+	},
+	{
+		id: 8,
+		url: "https://player.vimeo.com/video/1176080342",
+	},
+	{
+		id: 9,
+		url: "https://player.vimeo.com/video/1176080119",
+	},
+	{
+		id: 10,
+		url: "https://player.vimeo.com/video/1176080031",
+	},
 ];
