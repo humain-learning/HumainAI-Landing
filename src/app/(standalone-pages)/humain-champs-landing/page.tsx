49c447c2c3f180d9860e31c40dd8e0cb2b60d574
Raghav Kaul <raghav.kaul@humainlearning.ai>
2026-05-19 11:41:55 +0530

    update

--- src/app/(standalone-pages)/humain-champs-landing/page.tsx
+++ src/app/(standalone-pages)/humain-champs-landing/page.tsx
@@ -1,6 +1,12 @@
+import Hero from "@/components/hc-landing/Hero/Hero";
+import { Band } from "@/components/hc-landing/Band/Band";
+import { StudentCreations } from "@/components/hc-landing/StudentCreations/StudentCreations";
 export default function HumainChampsLanding() {
 	  return (
 		<>
+		<Hero />
+		<Band />
+		<StudentCreations />
 		</>
 	  )
 }
---
+++ src/components/hc-landing/Hero/Hero.tsx
@@ -0,0 +1,5 @@
+export default function Hero() {
+	return (
+		<div className="flex items-center justify-center h-screen">Hero section</div>
+	)
+}
