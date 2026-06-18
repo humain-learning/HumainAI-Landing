cf3e5e9c5d1ed1d72c715b0afd18660b92c29984
Raghav Kaul <raghav.kaul@humainlearning.ai>
2026-06-18 18:43:07 +0530

    Update CheckOutVersion2.tsx

--- src/components/checkout-page/CheckOutVersion2.tsx
+++ src/components/checkout-page/CheckOutVersion2.tsx
@@ -243,7 +243,7 @@ export const CheckOutV2 = ({templateDetails, availableBatches, billingDetails}: 
 						setOrderError("Payment cancelled.");
 					}
 				},
-				callback_url: `${window.location.origin}/checkout/confirmation?receipt=${order.receipt}&batchName=${selectedBatch?.name}&startDate=${selectedBatch?.itinerary[1].date}&courseName=${templateDetails.courseName}`,
+				callback_url: `https://humainlearning.ai/checkout/confirmation?receipt=${order.receipt}&batchName=${selectedBatch?.name}&startDate=${selectedBatch?.itinerary[1].date}&courseName=${templateDetails.courseName}`,
 				handler: async function (response: any) {
 					console.log("verifying payment with response:", response);
 					const res = await fetch('/api/verify-order', {
